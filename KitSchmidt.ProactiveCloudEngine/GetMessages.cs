d48e4bd8db6cd05306f954fc6c0635f644bc1480
Brian Voskerijian <Brian@intelligentdata.com>
2017-09-05 22:34:15 -0400

    Fix syntax error

--- KitSchmidt.ProactiveCloudEngine/GetMessages.cs
+++ KitSchmidt.ProactiveCloudEngine/GetMessages.cs
@@ -15,7 +15,7 @@ namespace KitSchmidt.ProactiveCloudEngine
             log.Info($"C# Timer trigger function executed at: {DateTime.Now}");
 
             var directLineSecret = "8mro3m2-O_0.cwA.kHQ.j6pa4HpvkyZVs-pNaT3ZXHjZdTY_26jAYC_2x1kDzPU";
-            var client = new DirectLineClient(secretOrToken: directLineSecret, );
+            var client = new DirectLineClient(secretOrToken: directLineSecret);
             var conversation = await client.Conversations.StartConversationAsync();
             var testActivity = new Activity
             {
